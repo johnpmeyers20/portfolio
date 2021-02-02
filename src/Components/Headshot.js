fef514bb20729149d9af122f5bd91e91e19b587b
JP Meyers <johnpmeyers20@yahoo.com>
2021-02-02 11:13:26 -0500

    added cv to media in src

--- src/Components/Headshot.js
+++ src/Components/Headshot.js
@@ -5,7 +5,7 @@ export default function Headshot() {
   return (
     <>
       <h2>Headshot</h2>
-      <Link><img src="https://i.imgur.com/fNV1sVc.png" alt="Portrait" width="200" height="auto"/></Link>
+      <Link><img src="https://i.imgur.com/YevAKpL.jpg" alt="Portrait" width="200" height="auto"/></Link>
     </>
   )
 }
